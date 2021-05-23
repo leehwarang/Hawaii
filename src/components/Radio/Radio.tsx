c4b057885b11c757587892f2ec6226b96086a687
hwaranglee <hwrng2@gmail.com>
2021-05-23 15:16:01 +0900

    refactor: RadioGroup 에서 as assertion 사용을 3회에서 1회로 줄임
    - as assertion이 필요한 요소에 여러번 접근해야 할 때 변수로 빼서 사용
    - ReactElement에 PropsWithChildren 으로 return 값의 타입을 명시적으로 선언

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ReactNode, ReactElement, createContext, useContext } from 'react'
+import React, { useState, useEffect, ReactNode, ReactElement, createContext, useContext, PropsWithChildren } from 'react'
 
 interface RadioGroupProps {
     children : React.ReactNodeArray
@@ -17,6 +17,7 @@ interface RadioGroupContextType {
 
 const RadioGroupContext = createContext({} as RadioGroupContextType)
 
+// TODO: states의 형태는 올바른가? value와 selected 를 가지는 객체가 아닌 selected 상태의 value만 가지고 있으면 어떨까? (find library)
 const RadioGroup = ({ children, onChange } : RadioGroupProps ) => {
     const [states, setStates ] = useState<State[]>([])
 
@@ -26,9 +27,12 @@ const RadioGroup = ({ children, onChange } : RadioGroupProps ) => {
     }
 
     useEffect(() => {
-     const result = React.Children.map(children, (c => ({ value : (c as ReactElement).props.value, selected : (c as ReactElement).props.initOnMount || false})))
-     setStates(result as State[])
+     const result = React.Children.map(children, (c => {
+        const item = c as ReactElement<PropsWithChildren<RadioItemProps>>
+        return { value : item.props.value, selected : item.props.initOnMount || false }
+     }))
 
+     setStates(result)
     }, [])
 
     return <RadioGroupContext.Provider value={{ states, handleChange}}>{children}</RadioGroupContext.Provider>
@@ -36,14 +40,14 @@ const RadioGroup = ({ children, onChange } : RadioGroupProps ) => {
 
 export default RadioGroup
 
-interface RadioProps {
+interface RadioItemProps {
     name: string
     value: string
     disabled ?: boolean
     initOnMount ?: boolean
 }
 
-export const Radio = ({ name, value, disabled } : RadioProps) => {
+export const Radio = ({ name, value, disabled } : RadioItemProps) => {
     const { states, handleChange } = useContext(RadioGroupContext)
     const self = states.find(state => state.value === value) as State
 
